d33bb9cb5c96e5e7130b99a9d302b8a87f104025
vitaly <vittar@yandex.ru>
2016-07-03 17:55:39 +0300

    5,2 шаг 12

--- main.cpp
+++ main.cpp
@@ -2,25 +2,31 @@
 #include <signal.h>
 #include <unistd.h>
 #include <fstream>
+#include <string>
+#include <sys/wait.h>
 
 using namespace std;
 
 void sig_handler(int res, siginfo_t *info, void *tmp){
-    printf("Вызов!");
+    int status;
+    waitpid(-1, &status, NULL);
 }
 
 int main(int argc, char *argv[]) {
-    struct sigaction sig_act;
-    sig_act.sa_handler = SIG_IGN;
-//    sig_act.sa_sigaction = sig_handler;
-    sig_act.sa_flags = SA_RESTART;
-    ofstream myfile ("/home/box/pid");
-    int pid = getpid();
-    myfile << pid;
+    string name="/home/box/pid_parent";
+    if (fork()){
+        name="/home/box/pid_child";
+    }
+    else{
+        struct sigaction sig_act;
+        sig_act.sa_sigaction = sig_handler;
+        sig_act.sa_flags = SA_RESTART;
+        sigaction(SIGCHLD, &sig_act, NULL);
+    }
+    ofstream myfile (name);
+    myfile << getpid();
     myfile.flush();
     myfile.close();
-    sigaction(SIGTERM, &sig_act, NULL);
-    sigaction(SIGINT, &sig_act, NULL);
     pause();
     return 0;
 }
