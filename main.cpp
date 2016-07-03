f2b0c1e04257cc4e2313eaf0a9581c611fa06038
vitaly <vittar@yandex.ru>
2016-07-03 22:17:16 +0300

    5,4 шаг 11

--- main.cpp
+++ main.cpp
@@ -7,26 +7,40 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 using namespace std;
 
+void child(int sock){
+    pause();
+}
+
+void parent(int sock) {
+    pause();
+
+}
+
 int main(int argc, char *argv[]) {
-    char buf[1024];
-    ///home/box/in.fifo и /home/box/out.fifo
-    char fifoin[] = "/home/box/in.fifo";
-    char fifoout[] = "/home/box/out.fifo";
-    if(mkfifo(fifoin, 0666) == -1){
+    int sv[2];
+    int pid;
+
+    if (socketpair(AF_LOCAL, SOCK_STREAM, 0, sv) < 0) {
+        perror("socketpair");
         exit(1);
     }
-    if(mkfifo(fifoout, 0666) == -1){
-        exit(1);
+    switch ((pid = fork())) {
+        case 0:
+            close(sv[0]);
+            child(sv[1]);
+            break;
+        case -1:
+            perror("fork");
+            return 1;
+        default:
+            close(sv[1]);
+            parent(sv[0]);
+            break;
     }
-    int fd_in = open(fifoin, O_RDONLY);
-    int fd_out = open(fifoout, O_WRONLY);
-    while(true){
-        ssize_t rb = read(fd_in, buf, 1024);
-        if (rb == -1)break;
-        write(fd_out, buf, rb);
-    };
     return 0;
 }
